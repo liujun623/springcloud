6f73216573c57844b5c5aa1f4862f2826c74b59d
lj <1602575772@qq.com>
2021-02-27 17:26:08 +0800

    项目的重构

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -1,4 +1,5 @@
 package com.atguigu.springcloud.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
